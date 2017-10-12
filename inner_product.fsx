d72f16b3518709e6a1ead2d9140f57a36d9dc2d9
Rolf Kinder Gilet <rgile002@fiu.edu>
2017-10-12 11:06:41 -0400

    Create inner_product.fsx

---
+++ inner_product.fsx
@@ -0,0 +1,11 @@
+// By Rolf Kinder Gilet 
+// takes two vectors represented as int lists and returns their inner product
+
+let rec innerproduct xs ys=
+    match xs,ys with
+    |[],[] -> 0
+    |[],ys -> 0
+    |xs,[] -> 0
+    |x::xs, y::ys -> (x*y) + innerproduct xs ys;;
+
+innerproduct [1;2;3][4;5;6];;
